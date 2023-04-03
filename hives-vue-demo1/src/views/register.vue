89151e3c635a49e7a60bfc013658620cc99c99ad
zhangwentao <2298805496@qq.com>
2023-04-03 20:37:19 +0800

    登陆注册前端功能实现

---
+++ hives-vue-demo1/src/views/register.vue
@@ -0,0 +1,114 @@
+<template>
+    <div class="loginbody">
+    <div class="logindata">
+      <div class="logintext">
+        <h2>用户注册</h2>
+      </div>
+      <div class="formdata">
+        <el-form ref="form" :model="form" :rules="rules">
+          <el-form-item prop="email" v-show="!this.next">
+            <el-input
+              v-model="form.email"
+              clearable
+              placeholder="请输入注册邮箱"
+            ></el-input>
+          </el-form-item>
+          <el-form-item prop="nickname" v-show="this.next">
+            <el-input
+              v-model="form.nickname"
+              clearable
+              placeholder="请输入昵称"
+            ></el-input>
+          </el-form-item>
+
+          <el-form-item prop="nickname" v-show="!this.next">
+            <el-input
+              v-model="form.code"
+              clearable
+              placeholder="请输入验证码"
+            ></el-input>
+          </el-form-item>
+
+          <el-form-item prop="password" v-show="this.next">
+            <el-input
+              v-model="form.password"
+              clearable
+              placeholder="请输入密码密码"
+              show-password
+            ></el-input>
+          </el-form-item>
+
+          <el-form-item prop="determine" v-show="this.next">
+            <el-input
+              v-model="form.determine"
+              clearable
+              placeholder="请再次输入密码"
+              show-password
+            ></el-input>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div class="butt">
+        <el-button type="primary" @click="login()"
+          >返回登录</el-button
+        >
+        <el-button class="shou" @click="validate">发送验证码</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "register",
+  data() {
+    return {
+      form: {
+        password: "",
+        email: "",
+        nickname: "",
+        determine: "",
+        code: ""
+      },
+      rules: {
+        email: [
+          { required: true, message: "请输入邮箱名", trigger: "blur" },
+          { max: 20, message: "不能大于20个字符", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { max: 20, message: "不能大于20个字符", trigger: "blur" },
+        ],
+        determine: [
+           { required: true, message: "请输入密码", trigger: "blur" },
+           { max: 20, message: "不能大于20个字符", trigger: "blur" },
+        ]
+      },
+      next: false
+    };
+  },
+  mounted() {
+  },
+  methods: {
+    login(form) {
+        this.$router.push("/")
+    },
+    remenber(data){    
+    },
+    forgetpas() {
+    },
+    validate(){
+        let params = {
+              "email": this.form.email
+        }
+        this.$http.get("/api/user/user/sendCode", {params}).then(res=>{
+            this.next=!this.next
+              alert("已发送验证码!")
+          })
+    },
+    register() {
+    },
+  },
+};
+</script>
