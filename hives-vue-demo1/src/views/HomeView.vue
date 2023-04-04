828f2fb4d8a259a3ece0dc84a2347b2030fa6638
xilinlan <73388383+xilinlan@users.noreply.github.com>
2023-04-04 16:14:01 +0800

    build lxl branch

--- hives-vue-demo1/src/views/HomeView.vue
+++ hives-vue-demo1/src/views/HomeView.vue
@@ -1,9 +1,37 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+
+</template>
+
+<script>
+
+
+export default {
+  props: [],
+  components: {},
+  data() {
+    return {
+    }
+  },
+  watch: {},
+  computed: {},
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  destroyed() {},
+  methods: {
+    request() {},
+    handleSelect() {},
+    hello() {
+      alert("来自预设逻辑代码的问候")
+    },
+  },
+  fillter: {},
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+
+</style>
